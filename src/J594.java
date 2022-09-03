788ea24705696053e873b4b0d4823a5235c4e29b
jaderain765 <leehw1412@naver.com>
2022-09-03 23:58:15 +0900

    Create J594.java

---
+++ src/J594.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class J594 {
+	public static void main(String[] args) {
+		String str;
+		Scanner sc = new Scanner(System.in);
+		System.out.println((str = sc.next())+ str);
+		sc.close();
+	}
+}
