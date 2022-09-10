98327d58b788d1c9ca35ddb52b2c94a0c8f27f2d
jaderain765 <leehw1412@naver.com>
2022-09-10 22:01:19 +0900

    커밋

---
+++ src/J606.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class J606 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.next()+"fighting");
+		sc.close();
+	}
+}
